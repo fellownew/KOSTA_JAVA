7f1e517a1f233eaa381d156bf25ebc64ce953a01
fellownew <hotttnew@naver.com>
2015-01-09 09:18:18 +0900

    HelloWorld.java
    헬로월드 파일 추가

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Random;
+
+public class HelloWorld{
+    public static void main(String args[]){
+		int a=(int)(Math.random()*100); // math.random은 0~1의 double형 난수
+		Random random = new Random();
+		int b = random.nextInt(1000);
+		System.out.println("Hello World - 안녕하세요");
+		System.out.println(a+","+b+","+(a+b));
+    }
+ }
